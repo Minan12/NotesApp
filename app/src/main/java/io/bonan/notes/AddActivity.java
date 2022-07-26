1409f2f8447bf8ed10409d5c43e3259ff2ce7b81
bondopangaji <60285086+bondopangaji@users.noreply.github.com>
2022-07-26 08:18:36 +0700

    refactor(AddActivity): fix persisting data strategy to firebase document structure defined

--- app/src/main/java/io/bonan/notes/AddActivity.java
+++ app/src/main/java/io/bonan/notes/AddActivity.java
@@ -10,50 +10,65 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import io.bonan.notes.model.NoteModel;
+import io.bonan.notes.util.DateUtil;
+
+import java.util.Objects;
 
 public class AddActivity extends AppCompatActivity {
     //Variable
-    TextInputLayout title, description, email, phone, age;
+    TextInputLayout titleTV, descriptionTV;
     Button SEND, btn_Back;
-
     FirebaseDatabase rootNode;
     DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
         //Hooks to all xml elements in activity_registrasi.xml
-        title = findViewById(R.id.TITLE);
-        description = findViewById(R.id.DESC);
+        titleTV = findViewById(R.id.TITLE);
+        descriptionTV = findViewById(R.id.DESC);
         SEND = findViewById(R.id.Send);
         btn_Back =findViewById(R.id.btnBack);
+        // get current logged-in user
 
         //Save data in firebase on button click
         SEND.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rootNode = FirebaseDatabase.getInstance("https://bonan-notes-default-rtdb.asia-southeast1.firebasedatabase.app/");
-                reference = rootNode.getReference("Notes");
+                // get all the value
+                String Title = Objects.requireNonNull(titleTV.getEditText()).getText().toString();
+                String Description = Objects.requireNonNull(descriptionTV.getEditText()).getText().toString();
+                String CreatedAt = DateUtil.dateNow();
 
-                //get all the value
-                String Title= title.getEditText().getText().toString();
-                String Description = description.getEditText().getText().toString();
+                // get current logged-in user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-                if(TextUtils.isEmpty(Title)){
-                    title.setError("FirstName cannot be empty");
-                    title.requestFocus();
-                }
-                else if(TextUtils.isEmpty(Description)){
-                    description.setError("LastName cannot be empty");
-                    description.requestFocus();
-                }
-                else {
-                    NoteModelActivity helperClass = new NoteModelActivity(Title, Description);
-                    reference.child(Title).setValue(helperClass);
-                    Toast.makeText(AddActivity.this, "Berhasil menambahkan", Toast.LENGTH_SHORT).show();
+                if (user != null) {
+                    // create db ref
+                    reference = rootNode.getReference("users");
+                    if(TextUtils.isEmpty(Title)){
+                        titleTV.setError("FirstName cannot be empty");
+                        titleTV.requestFocus();
+                    }
+                    else if(TextUtils.isEmpty(Description)){
+                        descriptionTV.setError("LastName cannot be empty");
+                        descriptionTV.requestFocus();
+                    }
+                    else {
+                        NoteModel helperClass = new NoteModel(Title, Description, CreatedAt);
+                        reference.child(user.getUid()).child("/notes").push().setValue(helperClass);
+                        Toast.makeText(AddActivity.this, "Berhasil menambahkan", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(AddActivity.this, "User doesn't exist", Toast.LENGTH_SHORT).show();
                 }
             }
         });
